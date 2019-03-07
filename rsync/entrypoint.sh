8c74a73aba2fe6e6ebdd05f7cb0067017161b600
Peter Gyima <peter.gyima@gimadigital.com>
2019-03-06 19:28:15 -0800

    Entry point added to rsync

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -11,7 +11,7 @@ rsync --daemon -4
 # run the commands every FREQ seconds
 while [ 1 ]
 do
-   if [ $# -eq 0 ] || [ -z "$1" ] || ! { [ -e "$1" ] && [ -f "$1" ] && [ -r "$1" ]; };
+   if [ $# -eq 0 ] || [ -z "$1" ] || ! { [ -e "$1" ] && [ -f "$1" ] && [ -r "$1" ] && [ -s "$1" ]; };
    then
       echo "commands file not available. sleeping forever ..."
       sleep 4512568 # sleep for a long time
